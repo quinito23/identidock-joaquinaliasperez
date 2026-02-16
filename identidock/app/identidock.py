46f3eb18eac24452b5074f2f81a0819320bd43f5
joaquin <quinoalias@gmail.com>
2026-02-16 13:36:35 +0100

    Paso 3 : Creacion del formulario

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,9 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
-
+default_name = 'Joaquin Alias'
 @app.route('/')
-def hello_world():
-	return 'Hello World!\n'
+def get_identicon():
+	name = default_name
+	header = '<html><head><title>Identidock</title></head><body>'
+	body = '''<form method="POST">
+		Hola <input type="text" name="name" value="{}">
+		<input type="submit" value="submit">
+		</form>
+		<p>Te pareces a:
+		<img src="/monster/monster.png"/>
+		'''.format(name)
+	footer = '</body></html>'
+	return header + body + footer
+
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
