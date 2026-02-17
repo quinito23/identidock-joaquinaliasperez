eb914679461d93688f9a610e0d9c267a8c836408
joaquin <quinoalias@gmail.com>
2026-02-17 12:14:16 +0100

    Paso 4 : Integracion con dnmonster funcionando

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,20 +1,34 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
 app = Flask(__name__)
-default_name = 'Joaquin Alias'
+salt = "UNIQUE_SALT"
+default_name = 'joaquin alias perez'
+
+
+
 @app.route('/')
-def get_identicon():
-	name = default_name
-	header = '<html><head><title>Identidock</title></head><body>'
-	body = '''<form method="POST">
-		Hola <input type="text" name="name" value="{}">
-		<input type="submit" value="submit">
-		</form>
-		<p>Te pareces a:
-		<img src="/monster/monster.png"/>
-		'''.format(name)
-	footer = '</body></html>'
-	return header + body + footer
+def mainpage():
+    name = default_name
+    header = '<html><head><title>Identidock</title></head><body>'
+    body = '''<form method="POST">
+        Hola <input type="text" name="name" value="{0}">
+        <input type="submit" value="submit">
+        </form>
+        <p>Te pareces a:
+        <img src="/monster/monster.png"/>
+        '''.format(name)
+    footer = '</body></html>'
+
+    return header + body + footer
+@app.route('/monster/<name>')
+def get_identicon(name):
+    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    image = r.content
+    return Response(image, mimetype='image/png')
 
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+
+    app.run(debug=True, host='0.0.0.0')
+
+
